13c9e28bed0fc58ff9c381bc8221dce5ad56b274
Caleb Smith <karagenit@outlook.com>
2018-05-08 10:36:45 -0400

    Use C Library

--- script.rb
+++ script.rb
@@ -1,53 +1,30 @@
 #!/usr/bin/env ruby
 
-##
-# Initial size to pool by
-# Divisor to break down by
-#
-def simulate(initial, breakdown, population = 8192, occurrence = 0.01)
-  population = Array.new(population, false)
-  population.map! { |e| rand < occurrence }
-  population = [population] #ugh
+require 'fiddle'
 
-  tests = 0
+libSimulate = Fiddle.dlopen('./libsimulate.so')
 
-  while population.any?
-#   p population
-#   puts "Tests: #{tests}"
-#   gets
-    for i in 0...population.length do
-      population[i] = population[i].each_slice(initial).to_a
-      (population[i].length - 1).downto(0).each do |index|
-        tests += 1
-        if population[i][index].any? # TODO: cleanup logic
-          if population[i][index].length == 1
-            population[i].delete_at(index)
-          end
-        else
-          population[i].delete_at(index)
-        end
-      end
-    end
-    population.flatten!(1)
-    initial = (initial / breakdown).round
-  end
+simulate = Fiddle::Function.new(
+  libSimulate['simulate'],
+  [Fiddle::TYPE_INT, Fiddle::TYPE_INT, Fiddle::TYPE_INT, Fiddle::TYPE_DOUBLE],
+  Fiddle::TYPE_INT
+)
 
-  return tests
-end
-
-SIMS = 50000
+POP  = 10000
+PROB = 0.01
+SIMS = 10000
 #VARS = [ [10000,10], [1000,10], [100,10], [10,10], [1,10], [10000, 2],
 VARS = [ [128,2], [100, 2], [88,2], [80,2], [64,2], [50, 2], [40,2], [32,2], [25,2], [24, 2], [20,2], [16,2] ]
 # missing comma here leads to bad initial/breakdown and TypeErrors in simulate
 
-puts "Simulating 1% of 8,192..."
+puts "Simulating #{(PROB * 100).round}% of #{POP}..."
 puts "Running each simulation #{SIMS} times..."
 
 VARS.each do |initial, breakdown|
   testsTotal = 0
   for i in 1..SIMS do
     print "Simulating... #{(100 * i / SIMS).to_i}%\r"
-    testsTotal += simulate(initial, breakdown)
+    testsTotal += simulate.call(initial, breakdown, POP, PROB)
   end
   puts "Staring at #{initial}...   \tdividing by #{breakdown}...\tAverage Tests: #{testsTotal/SIMS}"
 end
--- simulate.c
+++ simulate.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int simulate(int initial, int divisor, int population, double probability) {
-    printf("Population %d", initial);
+int containsAny(char *array, int length) {
+    for (int i = 0; i < length; i++) {
+        if (array[i]) return 1;
+    }
     return 0;
+}
+
+int containsAll(char *array, int length) {
+    for (int i = 0; i < length; i++) {
+        if (!array[i]) return 0;
+    }
+    return 1;
+}
+
+void fillArray(char *array, int length) {
+    for (int i = 0; i < length; i++) {
+        array[i] = 1;
+    }
+}
+
+int simulate(int initial, int divisor, int populationSize, double probability) {
+    // true/false array whether each person has trait
+    char *population = calloc(populationSize, sizeof(char));
+    for (int i = 0; i < populationSize; i++) {
+        population[i] = ((double)rand() / RAND_MAX) < probability;
+    }
+    // set each individual's index when they've been correctly ID'd
+    char *checked = calloc(populationSize, sizeof(char));
+
+    int tests = 0;
+
+    for (int groupSize = initial; groupSize > 0 && !containsAll(checked, populationSize);
+            groupSize = (groupSize / divisor) + (groupSize % divisor)) {
+
+        for (int index = 0; index < populationSize; index += groupSize) {
+            if (checked[index]) { // this group was checked before
+                // could skip more than current block size but oh well
+                continue;
+            }
+            tests += 1;
+            if (containsAny(population + index, groupSize)) { // TODO: cleanup logic
+                if (groupSize == 1) {
+                    checked[index] = 1;
+                }
+            } else {
+                fillArray(checked + index, groupSize);
+            }
+        }
+    }
+
+    return tests;
 }
