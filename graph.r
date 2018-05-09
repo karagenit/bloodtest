1792883eb7642f5c2444a7d227a94ed0ce9fd04d
Caleb Smith <karagenit@outlook.com>
2018-05-09 12:17:19 -0400

    Add Graph Info to Title
    Closes #7

--- graph.r
+++ graph.r
@@ -2,15 +2,20 @@
 
 args = commandArgs(TRUE)
 input = args[1]
-print(input)
 output = gsub("csv", "png", input)
+
+nums = regmatches(input, gregexpr("[0-9]+", input))
+sims = nums[[1]][1]
+pop = nums[[1]][2]
+prob = nums[[1]][3]
+div = nums[[1]][4]
+title = paste("Average Trials by Initial Group, ", sims, " Simulations\nN = ", pop,
+              ", P = ", prob, "%, Divisor: ", div, sep='')
 
 data <- read.csv(input, header=FALSE)
 
 x11()
-plot(data$V1, data$V3, type="n", 
-     main="Average Trials by Initial Group (100,000 Simulations)", 
-     xlab="Initial Group Size", ylab="Average Trials")
+plot(data$V1, data$V3, type="n", main=title, xlab="Initial Group Size", ylab="Average Trials")
 lines(data$V1, data$V3, type="o")
 
 dev.print(png, output, width=1080, height=600)
