6d4c00bb3d49c353558c07198d64215650c19fff
Caleb Smith <karagenit@outlook.com>
2018-05-09 07:57:44 -0400

    Add Unit Tests
    Closes #2

---
+++ test.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'fiddle'
+require 'test/unit'
+
+libSimulate = Fiddle.dlopen('./libsimulate.so')
+
+$simulate = Fiddle::Function.new(
+  libSimulate['simulate'],
+  [Fiddle::TYPE_INT, Fiddle::TYPE_INT, Fiddle::TYPE_INT, Fiddle::TYPE_DOUBLE],
+  Fiddle::TYPE_INT
+)
+
+$loadSim = Fiddle::Function.new(
+  libSimulate['setup'],
+  [],
+  Fiddle::TYPE_VOID
+)
+
+class TestSimulate < Test::Unit::TestCase
+  def setup()
+    $loadSim.call()
+  end
+
+  def test_zero()
+    assert_equal($simulate.call(100, 1, 100, 0), 1)
+    assert_equal($simulate.call(10, 1, 100, 0), 10)
+    assert_equal($simulate.call(1, 1, 100, 0), 100)
+  end
+
+  def test_one()
+    assert_equal($simulate.call(1, 1, 100, 1), 100)
+    assert_equal($simulate.call(10, 10, 100, 1), 110)
+    assert_equal($simulate.call(100, 100, 100, 1), 101)
+    assert_equal($simulate.call(100, 10, 100, 1), 111)
+  end
+end
